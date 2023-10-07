abdc2ebf06b43e5e89ebb620ebdbee7b51bed6cf
f <franklinrohan2002@gmail.com>
2023-10-08 00:10:02 +0530

    exploring stack using push and pop

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 int main()
 {
-    stack<char> ch;
-    stack.push(t);
-    stack.push(a);
-    stack.push(c);
+    stack<char> d;
+    d.push('t');
+    d.push('a');
+    d.push('c');
+
+    // cant use the classic itterator
+    // for (int i : d)
+    // {
+    //     cout << i << " ";
+    // }
+
+    // push and pop are the two common operation
+    cout << d.top() << endl;
+    d.pop();
+    cout << d.top() << endl;
+
+    cout << "is empty or not " << d.empty();
 }
