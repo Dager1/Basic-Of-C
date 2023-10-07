5ea2cd8ca0e423d6346f9773e5222d49a005928d
f <franklinrohan2002@gmail.com>
2023-10-07 23:05:57 +0530

    pushed valuse into list

--- List/List1.cpp
+++ List/List1.cpp
@@ -3,4 +3,14 @@
 using namespace std;
 int main()
 {
+    list<int> l;
+    l.push_back(9);
+    l.push_front(3);
+    l.push_front(2);
+
+    for (int i : l)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
 }
