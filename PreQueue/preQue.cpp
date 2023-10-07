78dd3f9ad809177389e32251f3ec24a82056650a
f <franklinrohan2002@gmail.com>
2023-10-08 00:46:50 +0530

    Made maximum and mimimum priority queue

---
+++ PreQueue/preQue.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{ // max heap
+    priority_queue<int> max;
+    // mim heap
+    priority_queue<int, vector<int>, greater<int>> mini;
+}
