480a7ac941bd3d95d074b5f5646460503515f0b3
f <franklinrohan2002@gmail.com>
2023-10-07 23:17:06 +0530

    copying of a list

--- List/List1.cpp
+++ List/List1.cpp
@@ -14,8 +14,18 @@ int main()
     }
     cout << endl;
 
+    // it delets what ever itterator it pointing to
     l.erase(l.begin());
 
+    for (int i : l)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // a an exiting list
+    list<int> n(l);
+    cout << "The new list n ";
     for (int i : l)
     {
         cout << i << " ";
