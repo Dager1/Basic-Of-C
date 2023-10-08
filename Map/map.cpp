7178ee4d93436855c5133ebc65d4dee36d29b675
f <franklinrohan2002@gmail.com>
2023-10-08 18:57:29 +0530

    insertion into the mapp

---
+++ Map/map.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main()
+{
+    map<int, string> m;
+
+    m[1] = "babbar";
+    m[2] = "love";
+    m[3] = "kumar";
+
+    for (auto i : m)
+    {
+        cout << i.first << endl;
+    }
+    cout << endl;
+    m.insert({6, "bell"});
+    for (auto i : m)
+    {
+        cout << i.first << endl;
+    }
+    cout << endl;
+}
