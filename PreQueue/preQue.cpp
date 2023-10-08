dab9fd404913c0c021637949d621925b51204e6a
f <franklinrohan2002@gmail.com>
2023-10-08 10:46:28 +0530

    using the max pre-Que

--- PreQueue/preQue.cpp
+++ PreQueue/preQue.cpp
@@ -6,4 +6,18 @@ int main()
     priority_queue<int> max;
     // mim heap
     priority_queue<int, vector<int>, greater<int>> mini;
+
+    max.push(1);
+    max.push(2);
+    max.push(3);
+    max.push(4);
+
+    cout << "The max-size: " << max.size() << endl;
+    int size = max.size();
+    for (int i = 1; i <= size; i++)
+    {
+        cout << max.top();
+        max.pop();
+    }
+    cout << endl;
 }
