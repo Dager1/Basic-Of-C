06b8c605c583e863945574db31e1c45b5b841535
f <franklinrohan2002@gmail.com>
2023-10-08 10:55:52 +0530

    using the mimimum pre-Que and is empty

--- PreQueue/preQue.cpp
+++ PreQueue/preQue.cpp
@@ -12,6 +12,7 @@ int main()
     max.push(3);
     max.push(4);
 
+    // it itterates by displaying the maximum value
     cout << "The max-size: " << max.size() << endl;
     int size = max.size();
     for (int i = 1; i <= size; i++)
@@ -20,4 +21,21 @@ int main()
         max.pop();
     }
     cout << endl;
+
+    // it itterates by displaing the minimum valus
+    mini.push(1);
+    mini.push(2);
+    mini.push(3);
+    mini.push(4);
+    cout << "The max-size: " << mini.size() << endl;
+    int sizee = mini.size();
+    for (int i = 1; i <= sizee; i++)
+    {
+        cout << mini.top();
+        mini.pop();
+    }
+    cout << endl;
+
+    // using the empty method to to check if emprty
+    cout << "Is the queeu emprty:" << (mini.empty() ? "Yes" : "No");
 }
