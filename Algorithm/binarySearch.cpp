2a335942ad6e29a64eaf456e8c59d909db0014b0
f <franklinrohan2002@gmail.com>
2023-10-13 16:58:57 +0530

    fix: modefied mid equation

--- Algorithm/binarySearch.cpp
+++ Algorithm/binarySearch.cpp
@@ -4,10 +4,11 @@ int binarySearch(int arr[], int size, int key)
 {
     int start = 0;
     int end = size - 1;
+    // int mid = (start + end) / 2;
+    int mid = start + (end - start) / 2;
 
     while (start <= end)
     {
-        int mid = (start + end) / 2;
 
         if (arr[mid] == key)
         {
@@ -21,7 +22,7 @@ int binarySearch(int arr[], int size, int key)
         {
             end = mid - 1;
         }
-        mid = (start + end) / 2;
+        mid = start + (end - start) / 2;
     }
     return -1;
 }
