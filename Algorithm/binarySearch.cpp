c2a7aea3b761a44369aaf8d110dc79cd1c60b917
f <franklinrohan2002@gmail.com>
2023-10-13 16:50:09 +0530

    simple binary serach program

---
+++ Algorithm/binarySearch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return -1;
+}
+int main()
+{
+    int bin[6] = {4, 15, 34, 56, 67, 78};
+    int ans = binarySearch(bin, 6, 67);
+    cout << ans;
+}
